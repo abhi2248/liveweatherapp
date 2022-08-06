06265dcf178a2966905a50267bfcdd9c272236e3
Abhineet <abhineetkr123@gmail.com>
2022-08-07 00:32:03 +0530

    Updates

---
+++ precache-manifest.595e7ea0c0a2b77cb27a12a9d446ef37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc1fd58e7874a157a1d3",
+    "url": "/liveweatherapp/static/css/main.bfeea287.chunk.css"
+  },
+  {
+    "revision": "bc1fd58e7874a157a1d3",
+    "url": "/liveweatherapp/static/js/main.bc1fd58e.chunk.js"
+  },
+  {
+    "revision": "780666e43a043cca8235",
+    "url": "/liveweatherapp/static/js/1.780666e4.chunk.js"
+  },
+  {
+    "revision": "ea930205417ca4445493",
+    "url": "/liveweatherapp/static/js/runtime~main.ea930205.js"
+  },
+  {
+    "revision": "2db88082a614b5ed3afd2bb5dcf9f67a",
+    "url": "/liveweatherapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/liveweatherapp/precache-manifest.595e7ea0c0a2b77cb27a12a9d446ef37.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/liveweatherapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
