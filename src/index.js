b7dec7a013c618fe3fa693a73f12422b6062caf7
Abhineet <abhineetkr123@gmail.com>
2022-08-06 23:58:25 +0530

    my first commit reactwebsite

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 //import "./index.css";
-import App from "./App";
+import App from "./weather/App";
 
-// const img1="https://picsum.photos/200/300";
-// const img2="https://picsum.photos/200/300";
-// const img3="https://picsum.photos/200/300";
-// const links="https://www.google.com";
+
 ReactDom.render(
 <>
 <App/>
-{/* <h1 contentEditable="true">hello world</h1>
-<p className="paragraph">this is mt first program</p>
-<h2>welcome you all</h2>
-<div className="img_div">
-<img src={img2}alt="random images"/>
-<img src={img3}alt="random images"/>
-<a href={links}target="blank">
-<img src={img1}alt="random images"/>
-</a>
 
-</div> */}
 </>,
 document.getElementById("root"));
